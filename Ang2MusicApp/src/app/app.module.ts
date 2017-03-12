542d544502f8769a620665e5271c6673e5b5b8ef
Pulkit219 <ascension1110@gmail.com>
2017-03-12 12:24:46 -0400

    ok

--- Ang2MusicApp/src/app/app.module.ts
+++ Ang2MusicApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 import { provideRoutes} from '@angular/router';
+import {Album} from '../Album';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
