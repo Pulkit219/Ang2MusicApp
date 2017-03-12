0212db010d84bf262aed7de5ae70c74aa297a4a3
Pulkit219 <ascension1110@gmail.com>
2017-03-12 19:57:53 -0400

    ok

--- Ang2MusicApp/src/app/components/artist/artist.component.ts
+++ Ang2MusicApp/src/app/components/artist/artist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MusicService } from '../../services/music.service';
 import { Artist } from '../../../Artist';
 import { Album } from '../../../Album';
+import { ActivatedRoute, Params }   from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-artist',
@@ -13,9 +15,21 @@ export class ArtistComponent implements OnInit {
 id:string;
 artist:Artist[];
 albums: Album[];
-  constructor(private _musicService: MusicService) { }
+
+
+  constructor(private _musicService: MusicService,
+              private _route:ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this._route.params
+.map(params => params['id'])
+.subscribe((id) => {
+  this._musicService.getArtist(id)
+  .subscribe(artist => {
+    this.artist = artist;
+  })
+})
   }
 
 }
