a06ba49546ce7d89fd176995290600866778b347
Pulkit219 <ascension1110@gmail.com>
2017-03-11 22:40:05 -0500

    commit

--- Ang2MusicApp/src/app/app.module.ts
+++ Ang2MusicApp/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routes } from './app.routes';
+import { provideRoutes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -18,7 +20,8 @@ import { SearchComponent } from './components/search/search.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
